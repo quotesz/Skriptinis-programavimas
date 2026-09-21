6fc5ddcbf4060654116eec5060dc1f08f12aba98
quotesz <krystianbogusevic@gmail.com>
2026-09-21 12:04:15 +0300

    L2 baigtas

--- LD2/LD2_1var_KrystianBogusevic_1.m
+++ LD2/LD2_1var_KrystianBogusevic_1.m
@@ -1,12 +1,12 @@
 x = 5:2:34;
 y = exp(x);
 z = x ./ y;
-rezultatas = z';
+rezultatas = z'
 
 M = [pi/2 3i; log(2) 2*pi];
 nauja_eilute = [exp(M(1,1)) exp(M(1,2))];
 M2 = [M; nauja_eilute];
-sumos = sum(M2, 2);
+sumos = sum(M2, 2)
 
 t = 0:0.001:1;
 amplitude = 5;
@@ -24,7 +24,7 @@ virs_ribos = s_triuksmas(s_triuksmas > U1);
 s_filtruotas = s_triuksmas;
 s_filtruotas(abs(s_filtruotas) < U2) = 0;
 
-dydis_viso = length(s_triuksmas);
-dydis_atrinktu = length(virs_ribos);
-didziausia = max(s_filtruotas);
-maziausia = min(s_filtruotas);
+dydis_viso = length(s_triuksmas)
+dydis_atrinktu = length(virs_ribos)
+didziausia = max(s_filtruotas)
+maziausia = min(s_filtruotas)
