6dc85b14e8acafe6df7ce283857ca7ca2d84b34a
quotesz <krystianbogusevic@gmail.com>
2026-09-21 09:05:50 +0300

    Atlikta papildoma dalis

--- LD1/LD1_KrystianBogusevic.m
+++ LD1/LD1_KrystianBogusevic.m
@@ -6,3 +6,12 @@ plot(x, y, 'o-r', x, y/3, 'xb')
 title('Dvi funkcijos')
 xlabel('X-ai')
 ylabel('F_1 [-o-]   ir   F_2 [-x-]')
+
+N = 0;
+v = (N+1):0.5:(N+4)
+A = [N N+1 N+2; N+3 N+4 N+5; N+6 N+7 N+8]
+a = A(3,2)
+b = A(2:3,1:2)
+c = A([1 3],[1 3])
+v1 = v(1:3)
+B = [A; v1]
